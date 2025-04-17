7ee1ca10bd81c458b423c6c3fc857652a8b4d204
jia <jiayiliu0720@gmail.com>
2025-04-17 16:21:32 +0800

    完成本地 build，準備部署

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'CurioJourney',
 
-  projectId: 'jlgmgsht',
-  dataset: 'production',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+  dataset: process.env.SANITY_STUDIO_DATASET!,
   plugins: [structureTool()],
 
   server: {
